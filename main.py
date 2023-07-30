dc36b6ef8a2fa7959511dfd98bd20805754c3070
aleksandr.stolbov <aleksandr.stolbov@libertexgroup.com>
2023-07-31 01:30:04 +0200

    fixed typo in env vars

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ ALLOW_GROUPS = [-927544591]
 
 storage = Storage(
     os.environ.get("DB_HOST"),
-    os.environ.get("DB_POSR"),
+    os.environ.get("DB_PORT"),
     os.environ.get("DB_USER"),
     os.environ.get("DB_PASSWORD"),
     os.environ.get("DB_NAME"),
