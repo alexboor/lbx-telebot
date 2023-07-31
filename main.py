804f769801c8c92f167eb0e1e73f20be261f5b1e
aleksandr.stolbov <aleksandr.stolbov@libertexgroup.com>
2023-07-31 13:58:22 +0200

    mmm

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import handler
 from storage import Storage
 
 API_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
-VERSION = "1.1.4"
+VERSION = "1.1.5"
 ALLOW_CHATS = [int(i) for i in os.environ.get("ALLOW_CHATS").split(",")]
 
 print(f"Allowed chats: {ALLOW_CHATS}")
@@ -28,13 +28,14 @@ def send_version(msg):
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
-    if ((message.chat.type == "group") or (message.chat.type == "supergroup")) and (message.chat.id in ALLOW_CHATS):
+    if message.chat.id in ALLOW_CHATS:
+        print(message)
         handler.word_counter(storage, message)
 
 
 # def listener(messages):
 #     for m in messages:
-#         print(m)
+#         print(m.chat)
 #
 #
 # bot.set_update_listener(listener)
