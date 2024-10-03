b12ddffe83f6fd7b44bb433a3b66d100dbb659a2
Alexandre <boorick@gmail.com>
2024-10-03 17:47:32 +0200

    remove unnecessary configs update packages

--- go.mod
+++ go.mod
@@ -3,13 +3,12 @@ module github.com/alexboor/lbx-telebot
 go 1.21
 
 require (
-	github.com/go-resty/resty/v2 v2.10.0
-	github.com/jackc/pgx/v4 v4.18.1
+	github.com/go-resty/resty/v2 v2.11.0
+	github.com/jackc/pgx/v4 v4.18.2
 	github.com/jdkato/prose/v2 v2.0.0
-	github.com/joho/godotenv v1.5.1
 	github.com/wcharczuk/go-chart/v2 v2.1.1
 	golang.org/x/exp v0.0.0-20230801115018-d63ba01acd4b
-	gopkg.in/telebot.v3 v3.1.3
+	gopkg.in/telebot.v3 v3.3.6
 )
 
 require (
@@ -17,18 +16,18 @@ require (
 	github.com/deckarep/golang-set v1.7.1 // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
-	github.com/jackc/pgconn v1.14.0 // indirect
+	github.com/jackc/pgconn v1.14.3 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
-	github.com/jackc/pgproto3/v2 v2.3.2 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.3 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgtype v1.14.0 // indirect
 	github.com/jackc/puddle v1.3.0 // indirect
 	github.com/mingrammer/commonregex v1.0.1 // indirect
-	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/crypto v0.23.0 // indirect
 	golang.org/x/image v0.11.0 // indirect
-	golang.org/x/net v0.17.0 // indirect
-	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
 	gonum.org/v1/gonum v0.7.0 // indirect
 	gopkg.in/neurosnap/sentences.v1 v1.0.6 // indirect
 )
--- internal/cfg/cfg.go
+++ internal/cfg/cfg.go
@@ -1,70 +1,63 @@
 package cfg
 
 import (
-    "fmt"
-    "log/slog"
-    "os"
-    "strconv"
-    "strings"
+	"fmt"
+	"log/slog"
+	"os"
+	"strconv"
+	"strings"
 )
 
 type Cfg struct {
-    Token        string
-    ChatGPTToken string
-    AllowedChats []int64
-    Dsn          string
-    GifUrls      []string
+	Token        string
+	ChatGPTToken string
+	AllowedChats []int64
+	Dsn          string
 }
 
 // New creates new app config
 func New() *Cfg {
-    return &Cfg{
-        Token:        os.Getenv("TELEGRAM_BOT_TOKEN"),
-        ChatGPTToken: os.Getenv("CHATGPT_TOKEN"),
-        AllowedChats: initAllowedChats(),
-        Dsn:          initDsn(),
-        GifUrls:      initGifUrls(),
-    }
+	return &Cfg{
+		Token:        os.Getenv("TELEGRAM_BOT_TOKEN"),
+		ChatGPTToken: os.Getenv("CHATGPT_TOKEN"),
+		AllowedChats: initAllowedChats(),
+		Dsn:          initDsn(),
+	}
 }
 
 func InitLogger() {
-    opts := &slog.HandlerOptions{AddSource: true, Level: slog.LevelInfo}
-    logger := slog.New(slog.NewJSONHandler(os.Stdout, opts))
-    slog.SetDefault(logger)
+	opts := &slog.HandlerOptions{AddSource: true, Level: slog.LevelInfo}
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, opts))
+	slog.SetDefault(logger)
 }
 
 // initAllowedChats reads env ALLOW_CHATS, split value by `,` and parses it from string to int
 func initAllowedChats() []int64 {
-    var result []int64
+	var result []int64
 
-    allowedChatsEnv := os.Getenv("ALLOW_CHATS")
-    for _, chat := range strings.Split(allowedChatsEnv, ",") {
-        if len(chat) > 0 {
-            id, err := strconv.ParseInt(chat, 10, 64)
-            if err != nil {
-                slog.Warn("failed to parse ALLOW_CHATS env", slog.Any("error", err))
-            }
+	allowedChatsEnv := os.Getenv("ALLOW_CHATS")
+	for _, chat := range strings.Split(allowedChatsEnv, ",") {
+		if len(chat) > 0 {
+			id, err := strconv.ParseInt(chat, 10, 64)
+			if err != nil {
+				slog.Warn("failed to parse ALLOW_CHATS env", slog.Any("error", err))
+			}
 
-            result = append(result, id)
-        }
-    }
+			result = append(result, id)
+		}
+	}
 
-    return result
+	return result
 }
 
 // initDsn reads DB_USER, DB_PASSWORD, DB_HOST, DB_PORT and DB_NAME envs and creates DSN for connecting to database
 func initDsn() string {
-    dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
-        os.Getenv("DB_USER"),
-        os.Getenv("DB_PASSWORD"),
-        os.Getenv("DB_HOST"),
-        os.Getenv("DB_PORT"),
-        os.Getenv("DB_NAME"),
-    )
-    return dsn
-}
-
-// initGifUrls reads GIF_URLS env and splits it into a slice
-func initGifUrls() []string {
-    return strings.Split(os.Getenv("GIF_URLS"), ",")
+	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+	return dsn
 }
