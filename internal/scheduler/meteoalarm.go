3c46f440a6ffdbf4c9be1a90414f2c37dda5c362
Aleksandr Stolbov <boorick@gmail.com>
2025-07-02 21:55:13 +0200

    small fix of error output

--- internal/scheduler/meteoalarm.go
+++ internal/scheduler/meteoalarm.go
@@ -3,6 +3,7 @@ package scheduler
 import (
 	"encoding/json"
 	"fmt"
+
 	"github.com/alexboor/lbx-telebot/internal"
 	"github.com/alexboor/lbx-telebot/internal/meteoalarm"
 )
@@ -11,13 +12,13 @@ func (s *Schedule) MeteoalarmTask() {
 
 	today, tomorrow, err := meteoalarm.Extract()
 	if err != nil {
-		fmt.Errorf("error extracting meteoalarm data: %s\n", err)
+		fmt.Printf("error extracting meteoalarm data: %s\n", err)
 		return
 	}
 
 	todayJSON, err := json.Marshal(today)
 	if err != nil {
-		fmt.Errorf("error marshalling today slice to JSON: %s\n", err)
+		fmt.Printf("error marshalling today slice to JSON: %s\n", err)
 		return
 	}
 
@@ -25,7 +26,7 @@ func (s *Schedule) MeteoalarmTask() {
 
 	tomorrowJSON, err := json.Marshal(tomorrow)
 	if err != nil {
-		fmt.Errorf("error marshalling tomorrow slice to JSON: %s\n", err)
+		fmt.Printf("error marshalling tomorrow slice to JSON: %s\n", err)
 		return
 	}
 
